07cb453cfb4d9217e3e2e6bca0f3015da380b038
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2024-09-27 14:44:46 -0300

    Add files via upload

---
+++ Códigos em C/CPF.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int num[20], num1 = 0, num2 = 0, soma1 = 0, resultado1 = 0, soma2 = 0, resultado2 = 0;
+    
+
+    for (int i = 0; i < 9; i++){
+        printf("Digite o %d numero: ", i+1);
+        scanf("%d", &num1);
+        num[i] = num1 * (i+1);
+    }
+
+    for (int i = 0; i < 9; i++){
+        soma1 += num[i];
+    }
+
+    
+    resultado1 = soma1 % 11;
+    system("cls");
+    printf("\nResultado: %d\n", resultado1);
+
+    for (int i = 0; i < 10; i++){
+        num[i] = 0;
+    }
+
+    for (int i = 0; i < 10; i++){
+        printf("Digite o %d numero: ", i+1);
+        scanf("%d", &num2);
+        num[i] = num2 * i;
+    }
+
+     for (int i = 0; i < 10; i++){
+        soma2 += num[i];
+    }
+
+    resultado2 = soma2 % 11;
+    printf("\n%d", resultado1);
+    printf("\n%d", resultado2);
+    return 0;
+}
